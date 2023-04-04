13992eb174d025335ded4533829371f8c31b547c
Zachary Radford <101583420+zachradf@users.noreply.github.com>
2023-04-04 08:42:31 -0400

    testing webhooks

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import BasicClass from './Classes/BasicClass.js';
+import BasicClass from './Classes/BasicClass.js';  
 import HierarchyClass from './Classes/HierarchyClass.js';
 import { testDataScatterPlot, treeMapData, testDataBarChart } from '../../../../../src/SampleData/chartData.js';
 import { options, options2, optionsForceDirected } from '../../../../../src/SampleData/optionsData.js';
@@ -17,13 +17,8 @@ const input2 = {
 const testObject = new BasicClass(['SCATTER', 'LINE', 'AREA'], input);
 const testObject2 = new HierarchyClass(['TREEMAP', 'SUNBURST'], input2);
 
-// console.log(testObject.graphArray);
-// testObject.addGraphs(["AREA", "LINE"]);
-// console.log(testObject.graphArray);
 testObject.addGraphs('PIE');// update data
 // testObject.updateInput(input2);
 // testObject.removeChart("SCATTER");
 // testObject.removeChart("AREA");
-// testObject2.removeChart("SUNBURST");
-// testObject2.updateInput
-// testObject2.addGraphs("FORCE");
+
