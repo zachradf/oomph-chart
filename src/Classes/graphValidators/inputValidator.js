2835677cbcde271217ad4c8ad1d75e6bf2ed9603
Rob Mosher <rob@robmosher.com>
2023-04-29 00:52:23 -0400

    simplified data validators
    Also deepened support for object-based validation.

--- src/Classes/graphValidators/inputValidator.js
+++ src/Classes/graphValidators/inputValidator.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-use-before-define */
 import InputTypes from '../graphTypes/inputTypes.js';
 
+import { validateArrayHelper, validateObjectHelper } from './inputValidatorHelpers.js';
+
 /**
  * Determines supported input types.
  * @param {*} input - Any type is supported, as this function will determine what is supported.
@@ -8,8 +10,6 @@ import InputTypes from '../graphTypes/inputTypes.js';
  */
 export function getCompatibleInputTypes(userInput) {
   // Edge case: no input is provided
-  // TODO for now only arrays are supported
-  // TODO add support to shallow (single element) and deep (all elements) checking
   if (!userInput || !Array.isArray(userInput) || userInput.length === 0) return [];
 
   const compatibleInputTypes = [];
@@ -17,7 +17,9 @@ export function getCompatibleInputTypes(userInput) {
 
   // Check each input type
   Object.keys(inputTypes).forEach((inputType) => {
-    if (isValidInput(userInput, inputType, inputTypes)) compatibleInputTypes.push(inputType);
+    if (isValidInput(userInput, inputTypes[inputType].dataFormat)) {
+      compatibleInputTypes.push(inputType);
+    }
   });
 
   if (compatibleInputTypes.length === 0) console.error('Error caught: No valid input was found.');
@@ -25,16 +27,8 @@ export function getCompatibleInputTypes(userInput) {
   return compatibleInputTypes;
 }
 
-function isValidInput(userInput, inputType, inputTypes) {
-  const userDataExcerpt = [userInput[0].x, userInput[0].y];
-  const expectedDataShape = inputTypes[inputType].dataFormat;
-
-  return validateDataShape(userDataExcerpt, expectedDataShape);
-}
-
 /**
  * Abstract wrapper for validating input via indirect recursive calls.
- * TODO potentially add support for more than array element as root.
  *
  * Usage example:
  * const expectedDataShape = [
@@ -46,39 +40,14 @@ function isValidInput(userInput, inputType, inputTypes) {
  *   { key1: 'string', key2: 'number' },
  * ];
  */
-function validateDataShape(inputData, expectedDataShape) {
-  return validateArrayHelper(inputData, expectedDataShape);
-}
+// TODO for now only arrays as input root are supported
+// TODO add support for shallow (single element) and deep (all elements) checking
+function isValidInput(input, dataFormat) {
+  const inputExcerpt = [input[0].x, input[0].y];
 
-/** Helper function. */
-function validateTypeHelper(input, expectedType) {
-  if (expectedType === 'date') {
-    return input instanceof Date;
-  }
+  // Validate data shape
+  if (Array.isArray(inputExcerpt)) return validateArrayHelper(inputExcerpt, dataFormat);
+  if (typeof inputData === 'object') return validateObjectHelper(inputExcerpt, dataFormat);
 
-  if (Array.isArray(expectedType)) {
-    return validateArrayHelper(input, expectedType);
-  }
-
-  if (expectedType === 'object') {
-    return typeof input === 'object' && input !== null && !Array.isArray(input);
-  }
-
-  // eslint-disable-next-line valid-typeof
-  return typeof input === expectedType;
-}
-
-/** Helper function. */
-function validateArrayHelper(inputArray, expectedTypes) {
-  if (!Array.isArray(inputArray) || inputArray.length !== expectedTypes.length) {
-    return false;
-  }
-
-  for (let i = 0; i < inputArray.length; i += 1) {
-    if (!validateTypeHelper(inputArray[i], expectedTypes[i])) {
-      return false;
-    }
-  }
-
-  return true;
+  return false;
 }
---
+++ src/Classes/graphValidators/inputValidatorHelpers.js
@@ -0,0 +1,60 @@
+/* eslint-disable no-use-before-define */
+
+/** Helper function. */
+export function validateArrayHelper(inputArray, expectedTypes) {
+  if (!Array.isArray(inputArray) || inputArray.length !== expectedTypes.length) {
+    return false;
+  }
+
+  for (let i = 0; i < inputArray.length; i += 1) {
+    if (!validateTypeHelper(inputArray[i], expectedTypes[i])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/** Helper function. */
+// TODO further testing/verification/integration of this function is needed
+export function validateObjectHelper(inputObject, expectedStructure) {
+  if (typeof inputObject !== 'object' || inputObject === null || Array.isArray(inputObject)) {
+    return false;
+  }
+
+  const inputKeys = Object.keys(inputObject);
+  const expectedKeys = Object.keys(expectedStructure);
+
+  if (inputKeys.length !== expectedKeys.length) {
+    return false;
+  }
+
+  for (let i = 0; i < inputKeys.length; i += 1) {
+    const key = inputKeys[i];
+    if (
+      !expectedKeys.includes(key)
+      || !validateTypeHelper(inputObject[key], expectedStructure[key])) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+/** Helper function. */
+export function validateTypeHelper(input, expectedType) {
+  if (expectedType === 'date') {
+    return input instanceof Date;
+  }
+
+  if (Array.isArray(expectedType)) {
+    return validateArrayHelper(input, expectedType);
+  }
+
+  if (expectedType === 'object') {
+    return validateObjectHelper(input, expectedType);
+  }
+
+  // eslint-disable-next-line valid-typeof
+  return typeof input === expectedType;
+}
