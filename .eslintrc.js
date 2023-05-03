04494f16c698a4ffa7c4074b10fe166d0ce7964a
Rob Mosher <rob@robmosher.com>
2023-05-03 08:01:27 -0400

    within eslint, enforced es6 and added d3 as global
     * Converted from json-based to js-based config file
     * Alphabetized config objects

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: 'airbnb-base',
+  globals: {
+    d3: 'readonly',
+  },
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 6,
+  },
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'max-len': 'warn',
+    'no-console': 'off',
+    'no-underscore-dangle': 'warn',
+    'no-use-before-define': 'off',
+  },
+};
